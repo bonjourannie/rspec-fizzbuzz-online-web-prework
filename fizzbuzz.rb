0d801d4ef9210cff5a65e2ab72705f4f3dacb58a
Annie Truex <artruex@gmail.com>
2019-07-10 22:59:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
  
  def fizzbuzz (num)
    
-  if num / 3 
+  if num % 3 == 0
     puts 'Fizz'
-  elsif num / 5
+  elsif num % 5 == 0 
     puts 'Buzz'
-  elsif num / 3 && num / 5
+  elsif num % 3 ==0 && num % 5 == 0
     puts 'FizzBuzz'
   else 
     puts 'nil'
